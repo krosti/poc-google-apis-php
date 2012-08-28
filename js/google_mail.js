38af2ac2e8da0ba43555136372687a36e5ee3052
”Fernando <ceafernando@gmail.com>
2012-08-28 10:31:46 -0300

    fix for continuous update

--- js/google_mail.js
+++ js/google_mail.js
@@ -1,51 +1,61 @@
+/*
+
+Ideas:
+[ ] Add update of data when the user moves de mouse pointer (like keep-alive)
+[ ] Update Page Title with unread emails -> "K12 (1)"
+[ ] Parameter for UserName and Password (securely)
+[ ] Google Api Contacts: show users k12 stored in user contacts with @gmail account
+*/
+
 gmail = {
 
 	_init: function(){
-		/*
-		*  ToDo: Pass UserName and PW (securely)
-		*/
 		var 
 			  data = null
 			, gmail_url = "https://mail.google.com/mail/feed/atom"
 			, gmail_response = '';
 
-		gmail.userLoggedIn();
-		gmail_response = app.urlToXML(gmail_url,function(e){
-			gmail.continuousUpdate( eval('('+e+')') , 7000);
+		app.urlToXML(gmail_url,function(e){
+			var data = eval('('+e+')');
+			gmail.getUnreadMails( data );
+			gmail.showUserLoggedIn( data );
+			gmail_response = data;
 		});
-		//data = JSON.stringify(app.feedToJson(gmail_xml));
-		
+
+		//continuous update every 20"
+		( gmail_response != 'undefined' && gmail.continuousUpdate(gmail_response,20000) )
 	},
 
-	userLoggedIn: function(data){
+	showUserLoggedIn: function(data){
 		var userBox = document.getElementById('userLogged');
-		//userBox.innerHTML = data.name;
+		userBox.innerHTML = '';
+		userBox.innerHTML = data.title;
 	},
 
 	getUnreadMails: function(data){
-		$('.userLogged').empty().append(data.title);
 		$('#unread_emails').empty().append(data.fullcount);
 		gmail.updateBox(data);
 	},
 
 	updateBox: function(data){
-				console.log(data);
 		var emails = data.entry
 			,	box = document.getElementById('emails')
 			,	email = document.createElement('span')
 			, 	count = 1;
 		box.innerHTML = '';
-		for (var i = emails.length - 1; i >= 0; i--) {
-			email.innerHTML += 
-				count++ + '- From: ' + emails[i].author.name + 
-				'<br>' + emails[i].title + ' | ' + emails[i].summary + '<div class="divisor"></div>';
-			email.setAttribute('id',emails[i].id);
-			console.log();
-			box.appendChild(email);
+		if (emails && emails.length){
+			for (var i = emails.length - 1; i >= 0; i--) {
+				email.innerHTML += 
+					count++ + '- From: ' + emails[i].author.name + 
+					'<br>' + emails[i].title + ' | ' + emails[i].summary + '<div class="divisor"></div>';
+				email.setAttribute('id',emails[i].id);
+				console.log();
+				box.appendChild(email);
+			};
 		};
 	},
 
 	continuousUpdate:function(data,interval){
-		setInterval(function(){gmail.getUnreadMails(data)},interval);
+		setInterval(function(){gmail._init(data)},interval);
 	}
 }
