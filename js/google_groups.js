22a062365080a2f876612c1fd0dcb37a7ae903e7
Fernando Cea <ceafernando@gmail.com>
2012-09-06 18:39:30 -0300

    users/intento de labels/google groups

--- js/google_groups.js
+++ js/google_groups.js
@@ -25,18 +25,27 @@ ggroups = {
 		
 		$.ajax({
 		  dataType: "json",
-		  //data: 'access_token='+gapi.auth.getToken().access_token,
+		  //data: 'access_token='+gapi.auth.getToken().access_token+"&access_token_type=oauth",
+		  data: {
+		  	"Method":"GET",
+		  	"absoluteURI":"https://apps-apis.google.com/a/feeds/group/2.0/borealdev.com.ar/",
+		  	"headers":{},
+		  	"message-body":"",
+		  	"access_token":gapi.auth.getToken().access_token,
+		  	"access_token_type":"oauth"
+		  },
 		  beforeSend: function(xhr){
 		  },
 		  crossDomain: true,
 		  jsonpCallback: app.callB,
-		  url: 'https://apps-apis.google.com/a/feeds/group/2.0/borealdev.com.ar',
+		  url: "https://apps-apis.google.com/a/feeds/group/2.0/borealdev.com.ar/",
 		  success: function(data, status, xhr) {
 		  	//ggroups.updateBox(data);
 		  	console.log(data);
 		  },
-		  error:function(){
+		  error:function(d){
 		  	console.log('Server is Down :(');
+		  	console.log(d);
 		  }
 		});
 	}
