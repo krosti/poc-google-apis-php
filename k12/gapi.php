fcb0949ea29662a878ffe4635bf0334832d65220
”Fernando <ceafernando@gmail.com>
2012-09-12 10:52:48 -0300

    deleted folder

--- k12/gapi.php
+++
@@ -1,9 +0,0 @@
-<?php
-//fucntion to get unread emails taking username and password as parametes
-
-$xmlString = file_get_contents("https://fernando.cea@globant.com:Cuadrilatero2@mail.google.com/mail/feed/atom");
-$xml = new SimpleXMLElement($xmlString);
-//$data = array('unread_emails' => $xml->fullcount);
-echo json_encode($xml);
-
-?>
--- k12/js/dev_tool.js
+++
@@ -1,8 +0,0 @@
-
-/*
-* shortcut for Console.log
-* resume: when you don't need show console logs just need change on your app __devTool to FALSE
-*/
-function __d(data){
-	(__devTool) ? console.log(data) : null;
-}
